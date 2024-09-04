f00cdaaf50e84dbea2606979ccda65701eca7113
NikitaTopchii <106115517+NikitaTopchii@users.noreply.github.com>
2024-09-04 05:24:20 +0300

    init app

---
+++ angular-task/src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import {AppComponent} from "./app.component";
+import {ParentComponent} from "./parent/parent.component";
+
+const routes: Routes = [
+  {
+    path: '',
+    component: ParentComponent,
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
